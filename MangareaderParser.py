594afce55c66af654f9a9720cb638a9a6bf26afa
Arnaud TANGUY <arn.tanguy@gmail.com>
2012-03-30 23:14:33 +0200

    Begin parsing manga list

--- MangareaderParser.py
+++ MangareaderParser.py
@@ -1,20 +1,30 @@
-import Parser
+from Parser import Parser
+from Network import Network
+from BeautifulSoup import BeautifulSoup
 
 class MangareaderParser(Parser):
     def __init__(self, baseUrl):
         Parser.__init__(self, baseUrl)
         print("Mangareader Parser initialized")
 
-    def getMangaList(relativeUrl):
+    def getMangaList(self, relativeUrl):
         """ Returns a dictionnary containing the values as key: manga name, value: manga url """
+        html = self.network.getUrlContentUtf8("http://www.mangareader.net/alphabetical")
+        #self.baseUrl+relativeUrl)
+
+        self.soup = BeautifulSoup(html)
+        self.soup.findAll('ul')
+        print (self.soup.prettify())
         return {'Test', 'http://test.com'}
 
-    def getChapterList(relativeUrl):
+    def getChapterList(self, relativeUrl):
         """ Returns a dictionnary containing the values as key: chapter title, value: chapter url """
         return {}
 
-    def getChapterImagesUrls(relativeUrl):
+    def getChapterImagesUrls(self, relativeUrl):
         """ Returns a dictionnary containing the values as key: image name, value: image url """
         return {}
 
-parser = MangareaderParser("http://mangareader.net")
+parser = MangareaderParser("www.mangareader.net/")
+parser.getMangaList("alphabetical")
+
